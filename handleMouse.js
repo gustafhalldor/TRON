715815c0361faf98a14a917e835086a9273424c1
gustafhalldor <gustafh@gmail.com>
2015-11-19 01:08:49 +0000

    Now possible to select game modes with mouse
    \o/

--- handleMouse.js
+++ handleMouse.js
@@ -15,16 +15,37 @@ var g_mouseX = 0,
     g_mouseY = 0;
 
 function handleMouse(evt) {
-    
-    g_mouseX = evt.clientX - g_canvas.offsetLeft;
-    g_mouseY = evt.clientY - g_canvas.offsetTop;
-    
+
+    var rect = g_canvas.getBoundingClientRect();
+
+    g_mouseX = Math.round((evt.clientX - rect.left) / (rect.right - rect.left) * g_canvas.width);
+    g_mouseY = Math.round((evt.clientY - rect.top) / (rect.bottom - rect.top) * g_canvas.height);
+
     // If no button is being pressed, then bail
-    var button = evt.buttons === undefined ? evt.which : evt.buttons;
-    if (!button) return;
-    
+//    var button = evt.buttons === undefined ? evt.which : evt.buttons;
+//    if (!button) return;
+
+    // Mouse clicking PvP
+    if(g_mouseX > 98 && g_mouseX < 192 && g_mouseY > 454 && g_mouseY < 489) {
+        playmode = 1;
+    }
+
+    // Mouse clicking normal play
+    if(g_mouseX > 202 && g_mouseX < 295 && g_mouseY > 454 && g_mouseY < 489) {
+        playmode = 2;
+    }
+
+    // Mouse clicking Snake
+    if(g_mouseX > 304 && g_mouseX < 399 && g_mouseY > 454 && g_mouseY < 489) {
+        playmode = 3;
+    }
+
+    // Mouse clicking level play
+    if(g_mouseX > 407 && g_mouseX < 502 && g_mouseY > 454 && g_mouseY < 489) {
+        playmode = 4;
+    }
 }
 
 // Handle "down" and "move" events the same way.
 window.addEventListener("mousedown", handleMouse);
-window.addEventListener("mousemove", handleMouse);
+//window.addEventListener("mousemove", handleMouse);
